53bc99458291df182d434cce3e0d3bd8f643f0ff
maggosh <caberwikijack@gmail.com>
2014-06-24 16:12:12 -0400

    Changed target to #frontimage

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#frontimage").click(function() {
-        $("#intro").animate({top:'-100px'}, {queue:false, duration:500});
+        $(this).animate({top:'-100px'}, {queue:false, duration:500});
     });
 });
