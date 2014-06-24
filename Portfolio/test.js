97c0e5a7ac4b1388c13fa672e97e9947bed99f03
maggosh <caberwikijack@gmail.com>
2014-06-24 17:28:18 -0400

    Added window.load and document.ready

--- Portfolio/test.js
+++ Portfolio/test.js
@@ -1,5 +1,7 @@
-function() {
-    $("#frontimage").on("click",function() {
-        $(this).animate({top:'-100px'}, {queue:false, duration:500});
+$(window).load(function() {
+    $(document).ready(function() {
+        $("#frontimage").on("click",function() {
+            $(this).animate({top:'-100px'}, {queue:false, duration:500});
+        });
     }
 });
