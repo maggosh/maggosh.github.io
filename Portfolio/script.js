16ecb65b9c1e43067b41b1133914394d5c5312b0
maggosh <caberwikijack@gmail.com>
2014-06-24 16:09:29 -0400

    Edited if/else to test animation

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
     $("#frontimage").click(function() {
-        if($("#intro").css("top") == "-100px"){
-            $("#intro").animate({top:'0px'}, 500);
-        }
-        else {
-            $("#intro").animate({top:'-100px'}, 500);
-        }
+        $("#intro").animate({top:'-100px'}, 500);
     });
 });
