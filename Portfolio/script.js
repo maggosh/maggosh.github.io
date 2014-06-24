a118196fe5ca438df416d24fb787ccb9d5e0a40a
maggosh <caberwikijack@gmail.com>
2014-06-24 16:10:43 -0400

    Edited if/else to test animation again

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#frontimage").click(function() {
-        $("#intro").animate({top:'-100px'}, 500);
+        $("#intro").animate({top:'-100px'}, {queue:false, duration:500});
     });
 });
